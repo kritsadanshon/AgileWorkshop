6164fad8da095a3c93d672b5913f2f9e74544144
fireboat <fireboat999@hotmail.com>
2016-02-26 16:05:24 +0700

    Change mysql query code

--- api/get_list_account.php
+++ api/get_list_account.php
@@ -3,9 +3,9 @@
  
 	var account_id = $GET["id"];
 	
-	$query_string = "SELECT * FROM `ACCOUNT`;";
-	$result_query = $link->query($query_string);
-	while($row = $result_query->fetch_assoc()){
+	$query_string = "SELECT `account_id`,`account_name`,`bank_code`,`acc_type`,`logo_path` FROM `ACCOUNT`;";
+	$result_query = mysqli_query($link, $query_string);
+	while($row = mysqli_fetch_assoc($result_query)){
 		var_dump($row);
 	}
 ?>
