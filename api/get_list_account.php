088a6895c8f09cc6c0ad88347b221496225b0b1c
fireboat <fireboat999@hotmail.com>
2016-02-26 16:35:57 +0700

    remove get 'id' parameter from get_list_account.php

--- api/get_list_account.php
+++ api/get_list_account.php
@@ -3,7 +3,7 @@
  
 	$account_id = $_GET["id"];
 	
-	$query_string = "SELECT * FROM account WHERE account_id=".$account_id;
+	$query_string = "SELECT * FROM account;
 	$result_query = mysqli_query($link, $query_string);
 	while($row = mysqli_fetch_assoc($result_query)){
 		echo json_encode($row);
