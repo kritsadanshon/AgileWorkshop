70181d13f3ab2209c3a4510f0329ec1b90bd611f
fireboat <fireboat999@hotmail.com>
2016-02-26 16:21:18 +0700

    remove '`' char from query string

--- api/get_list_account.php
+++ api/get_list_account.php
@@ -3,7 +3,7 @@
  
 	$account_id = $_GET["id"];
 	
-	$query_string = "SELECT * FROM `account`";
+	$query_string = "SELECT * FROM account";
 	$result_query = mysqli_query($link, $query_string);
 	while($row = mysqli_fetch_assoc($result_query)){
 		echo $row['account_id'];
