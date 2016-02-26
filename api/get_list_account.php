3d0a7ea843aed42097048a5cc529b65642f52509
fireboat <fireboat999@hotmail.com>
2016-02-26 16:19:25 +0700

    change select all field from DB in get_list_account.php

--- api/get_list_account.php
+++ api/get_list_account.php
@@ -3,9 +3,9 @@
  
 	$account_id = $_GET["id"];
 	
-	$query_string = "SELECT `account_id`,`account_name`,`bank_code`,`acc_type`,`logo_path` FROM `account`";
+	$query_string = "SELECT * FROM `account`";
 	$result_query = mysqli_query($link, $query_string);
 	while($row = mysqli_fetch_assoc($result_query)){
-		var_dump($row);
+		echo $row['account_id'];
 	}
 ?>
