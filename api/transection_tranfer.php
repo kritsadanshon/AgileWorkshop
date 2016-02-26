e12c9adddd4e0f8944a46f7fef839b5d271cceb5
enjizen <enjizen.r@gmail.com>
2016-02-26 16:30:33 +0700

    api ต่าง ธนาคาร

--- api/transection_tranfer.php
+++ api/transection_tranfer.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include './connect.php';
 
@@ -18,53 +18,69 @@ $calAmount = 0;
 
 $result = mysqli_query($link, "SELECT bank_code FROM account WHERE account_id = '$accountNumberTo'");
 //print_r($result);
-while ($arr = mysqli_fetch_assoc($result)){
+while ($arr = mysqli_fetch_assoc($result)) {
     $bankCode = $arr['bank_code'];
 }
 
 $result = mysqli_query($link, "SELECT  remaining_amount FROM account WHERE account_id = '$accountNumberForm'");
 //print_r($result);
-while ($arr = mysqli_fetch_assoc($result)){
+while ($arr = mysqli_fetch_assoc($result)) {
     $remainingAmount = $arr['remaining_amount'];
 }
 
 //echo $bankCode;
 
-if($bankCode == '629'){
+if ($bankCode == '629') {
     $fee = 0;
-    
+
     $calAmount = $remainingAmount - $fee - $moneyTranfer;
-  $sql =  "UPDATE account SET remaining_amount = $calAmount WHERE account_id = '".$accountNumberForm."'"  ;
+    $sql = "UPDATE account SET remaining_amount = $calAmount WHERE account_id = '" . $accountNumberForm . "'";
 //  echo $sql;
-    
+
     mysqli_query($link, $sql);
-    
-    
+
+
     $sql = "INSERT INTO transaction VALUES(null,'$accountNumberForm','$accountNumberTo',$fee,$moneyTranfer,sysdate())";
     mysqli_query($link, $sql);
-    
-    
+
+
 //    $sql = 'SELECT max(trancation_id) as id FROM transaction';
 //    echo $sql;
     $result = mysqli_query($link, "SELECT MAX(transaction_id) as id FROM transaction");
-   
-     
-     
-        while ($arr = mysqli_fetch_assoc($result)){
-                 $id = $arr['id'];
-            }
-            
-            
-            echo json_encode($id);
 
-}
-else{
+
+
+    while ($arr = mysqli_fetch_assoc($result)) {
+        $id = $arr['id'];
+    }
+
+
+    echo json_encode($id);
+} else {
     $fee = 15;
-            
-  $calAmount = $remainingAmount - $fee;
+
+    $calAmount = $remainingAmount - $fee - $moneyTranfer;
+    $sql = "UPDATE account SET remaining_amount = $calAmount WHERE account_id = '" . $accountNumberForm . "'";
+//  echo $sql;
+
+    mysqli_query($link, $sql);
+
+
+    $sql = "INSERT INTO transaction VALUES(null,'$accountNumberForm','$accountNumberTo',$fee,$moneyTranfer,sysdate())";
+    mysqli_query($link, $sql);
+
+
+//    $sql = 'SELECT max(trancation_id) as id FROM transaction';
+//    echo $sql;
+    $result = mysqli_query($link, "SELECT MAX(transaction_id) as id FROM transaction");
+
+
+
+    while ($arr = mysqli_fetch_assoc($result)) {
+        $id = $arr['id'];
+    }
+
+
+    echo json_encode($id);
 }
-
-
-
-
 ?>
